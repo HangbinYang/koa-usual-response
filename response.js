503f5f4cd2577da32136506dfa0964aa829bf881
YangHangbin <yanghangbin@gmail.com>
2017-10-25 15:21:05 +0800

    init

---
+++ response.js
@@ -0,0 +1,40 @@
+'use strict'
+
+module.exports = () => {
+  return async (ctx, next) => {
+
+    ctx.success = data => {
+      ctx.status = 200;
+      ctx.body = {
+        status: 200,
+        data: data || 'ok'
+      }
+    }
+
+    ctx.bad = error => {
+      ctx.status = 200;
+      ctx.body = {
+        status: 0,
+        message: error || 'bad request'
+      }
+      console.error({
+        status: 0,
+        message: error || 'bad request',
+        context: JSON.stringify(ctx)
+      })
+    }
+
+    ctx.error = error => {
+      ctx.status = 500;
+      ctx.body = {
+        status: 0,
+        message: error || 'bad request'
+      }
+      console.error({
+        status: 500,
+        message: error || 'bad request',
+        context: JSON.stringify(ctx)
+      })
+    }
+  }
+}
